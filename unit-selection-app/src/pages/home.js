b43f57dc83539ab53e12164dfd1a792d961450da
jong0029 <jong0029@student.monash.edu>
2021-03-07 14:20:17 +1100

    added new section to home

--- unit-selection-app/src/pages/home.js
+++ unit-selection-app/src/pages/home.js
@@ -4,12 +4,15 @@ import React from 'react';
 export default function Home(){
     return (
         <section className="container home">
-            <div className="row">
+            <div className="section-hero">
                 <h1>Make <span>Unit Selection</span> Easy</h1>
                 <h4>Scheduler your time at Monash University and make selecting units stress-free</h4>
                 <button className="btn home-fst-btn" type="button">Making New Schedule</button>
                 <br></br>
                 <button className="btn home-snd-btn" type="submit">Upload Previous Schedule</button>
+            </div>
+            <div className="section-about">
+                <h1>About</h1>
             </div>
         </section>
     )
