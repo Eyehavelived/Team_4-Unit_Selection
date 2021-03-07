b0ea8806e2eec525e32339b5c5f675b42ce2a3c2
Jay Tan <email.jingjie@gmail.com>
2021-03-07 15:11:05 +1100

    Created js files for apollo server and schemas

---
+++ unit-selection-app/graphql/app.js
@@ -0,0 +1,16 @@
+const koa = require('koa')
+const {ApolloServer} = require('apollo-server-koa')
+const {makeExecutableSchema} = require('graphql-tools')
+const {resolvers, typeDefs} = require('./schemas')
+const PORT = 4000
+
+const server = new ApolloServer({
+    schema: makeExecutableSchema({typeDefs, resolvers})
+})
+
+const app = new koa()
+server.applyMiddleware({app})
+
+app.listen({ port: PORT}, () => {
+    console.log(`Server ready at http://localhost:${PORT + server.graphqlPath}`)
+})
