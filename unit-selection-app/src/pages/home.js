198ce4ac8c6cbdfccb5a1fec3512567c79845dd6
jong0029 <jong0029@student.monash.edu>
2021-03-10 15:13:26 +1100

    Moved home css into own file instead of cluttering base, fixed button styles for home and nav bar

--- unit-selection-app/src/pages/home.js
+++ unit-selection-app/src/pages/home.js
@@ -4,18 +4,27 @@ import Navigation from '../components/common/navigation';
 
 export default function Home(){
     return (
-        <section className="container home">
-            <div className="section-hero">
-                <Navigation/>
-                <h1>Make <span>Unit Selection</span> Easy</h1>
-                <h4>Scheduler your time at Monash University and make selecting units stress-free</h4>
-                <button className="btn home-fst-btn" type="button">Making New Schedule</button>
-                <br></br>
-                <button className="btn home-snd-btn" type="submit">Upload Previous Schedule</button>
-            </div>
-            <div className="section-about">
-                <h1>About</h1>
-            </div>
-        </section>
+        <article>
+            <section className="container-hero">
+                <div className="section-hero">
+                    <div className="nav">
+                        <Navigation/>
+                    </div>
+                    <div className="hero-content">
+                        <h1>Make <span>Unit Selection</span> Easy</h1>
+                        <h4>Schedule your time at Monash University and make selecting units stress-free.</h4>
+                        <button className="btn impact-btn home-fst-btn" type="button">Making New Schedule</button>
+                        <br></br>
+                        <button className="btn impact-btn home-snd-btn" type="submit">Upload Previous Schedule</button>
+                    </div>
+                    
+                </div>
+            </section>
+            <section className="container-about">
+                <div className="section-about">
+                    <h1>About</h1>
+                </div>
+            </section>
+        </article>
     )
 }
