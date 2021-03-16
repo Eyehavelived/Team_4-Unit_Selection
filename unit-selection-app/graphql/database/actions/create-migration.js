6bc50bbfd5248173b4bcd4b5d74d19c4143a47f4
Jay Tan <email.jingjie@gmail.com>
2021-03-17 01:10:35 +1100

    Added command to package.json for creating tables. Added script for connecting the db. Added script for creating migrations.

---
+++ unit-selection-app/graphql/database/actions/create-migration.js
@@ -0,0 +1,20 @@
+const fs = require('fs')
+const path = require('path')
+const { argv } = require('process')
+
+let tableName
+
+try {
+    tableName = process.argv.find(
+        (arg) => arg.includes('--tableName')
+        ).split('=')[1]
+} catch(err) {
+    return console.error('--tableName parameter not found. Please specify a table name.')
+}
+
+const fileName = new Date().getTime() + `_${tableName}.sql`
+
+fs.writeFile(path.resolve(__dirname, `../migrations/${fileName}`), '', (err) => {
+    if (err) throw new Error(err)
+    console.log('Created new migration in migrations folder.')
+})
