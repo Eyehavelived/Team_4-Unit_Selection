7b47b08335abe638ab0ee0d7b57350dd1f7dd4cd
YangtianYan <237208175qq.com>
2021-04-09 16:29:43 +1000

    Finish the requirement for filtered results of my units should show the unit name

--- unit-selection-app/src/components/createArea.js
+++ unit-selection-app/src/components/createArea.js
@@ -26,11 +26,15 @@ export default function CreateArea(props) {
     return (
       <ul className="list-unstyled my-1">
         {props.unitList.map((unit)=>
+        <div className="mb-2">
           <li>
             <input onClick={submitUnit(unit)} type="checkbox"
               id={unit.unitCode} className="mt-1"/>
-            <label for={unit.unitCode} className="ms-2 my-0">{unit.unitCode}</label>
+            <label for={unit.unitCode} className="ms-2 my-0">{unit.unitCode}
+            </label>
           </li>
+          <h7>{unit.unitName}</h7>
+          </div>
         )}
       </ul>
 
