4c1dbc3b753124e0ad17bf2ebeaf2e33be7d31d5
jong0029 <jong0029@student.monash.edu>
2021-05-06 00:29:50 +1000

    Fixed schdeule floating button; fixed space between sections in home; added link to hero-button; added smooth scroll to nav

--- unit-selection-app/src/components/scheduleForm.js
+++ unit-selection-app/src/components/scheduleForm.js
@@ -3,15 +3,17 @@ import {Row} from "react-bootstrap";
 
 export default function ScheduleForm(props){
 
+    const year = new Date().getFullYear();
+    const yearOptions = [year-6,year-5,year-4,year-3,year-2,year-1,year,year+1,year+2,year+3,year+4,year+5,year+6]
+    const semOptions = ["1","2","Summer A","Summer B","Winter"]
+
     const [teachingPeriod,setTeachingPeriod] = useState({
-        year:"1",
+        year:year,
         sem:"1",
         units:[]
     });
 
-    const year = new Date().getFullYear();
-    const yearOptions = [year-6,year-5,year-4,year-3,year-2,year-1,year,year+1,year+2,year+3,year+4,year+5,year+6]
-    const semOptions = ["1","2","summera","summerb","winter"]
+    
 
     function handleChange(event){
         const {name,value} = event.target;
