bf55db2e58e14f68f895e5a2dc1a7ff1df68c44c
Jay Tan <email.jingjie@gmail.com>
2021-05-14 16:03:30 +1000

    Added conditional to unitListCard semester so that if semester is not found, the error is passed silently. Passed unit semester to unitListCard in ScheduleCard. Refactored out console.log in Schedule

--- unit-selection-app/src/pages/schedule.js
+++ unit-selection-app/src/pages/schedule.js
@@ -194,7 +194,6 @@ export default function Selection(){
                                             <Draggable key={unit.unitCode} draggableId={unit.unitCode} index={index}>
                                                 {(provided)=>(
                                                 <div {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef}>
-                                                    {console.log(unit.unitSem)}
                                                    <UnitListCard code={unit.unitCode} name={unit.unitName} sem={unit.unitSem}/>
                                                 </div>
                                                 )}
